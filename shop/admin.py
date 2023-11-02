03a52349d44f4ffce1b3e4626ed204f8187a25d3
grahamcrackerslover <>
2023-11-03 01:39:38 +0300

    copy items

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Item, Purchase
 
+def copy_items(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.pk = None  # Clear the primary key to create a new object
+        obj.save()
+
+copy_items.short_description = 'Copy selected items'
+
+class ItemAdmin(admin.ModelAdmin):
+    actions = [copy_items]
+
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Purchase)
