dc05d0e2e34e8d2be261d5f613f09852ddb806f9
grahamcrackerslover <>
2023-11-03 01:15:39 +0300

    fixed review datetime

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,6 +3,13 @@ from .models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    created_at = serializers.SerializerMethodField()
+
     class Meta:
         model = Review
         fields = '__all__'
+
+    def get_created_at(self, obj):
+        # Format the date to the desired format
+        return obj.created_at.strftime('%d.%m.%Y')
+    
